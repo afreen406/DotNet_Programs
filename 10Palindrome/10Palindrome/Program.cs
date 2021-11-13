83771864aed97bdb010b8d0b33574d6e1628ec4c
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:03:58 +0530

    Add files via upload

---
+++ 10Palindrome/10Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reversed = 0, remainder, original;
+            Console.Write("Enter an integer: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            original = n;
+
+            while (n != 0)
+            {
+                remainder = n % 10;
+                reversed = reversed * 10 + remainder;
+                n /= 10;
+            }
+
+            if (original == reversed)
+                Console.Write("{0} is a palindrome.", original);
+            else
+                Console.Write("{0} is not a palindrome.", original);
+        }
+    }
+}
