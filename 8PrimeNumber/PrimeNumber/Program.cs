0d15915220ee59f1f55c43d777be244877a4b2d9
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:01:58 +0530

    Add files via upload

---
+++ 8PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeNumber
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            int num;      
+            Console.Write("Enter the number\n");
+            num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;       
+
+            for (int i = 2; i < num; i++)  
+            {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count != 0)           
+            {
+                Console.Write("{0} is Not a prime number\n",num);
+            }
+            else
+            {
+                Console.Write("{0} is Prime number\n",num);
+            }
+        }
+    }
+}
