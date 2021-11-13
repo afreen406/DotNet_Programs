530d3362a25ccbb92dbcdaa454438e12b1b00bd2
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:02:50 +0530

    Add files via upload

---
+++ 10BinaryOnes/10BinaryOnes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DotNetTutorials
+{
+    public class CountNoOf1Bits
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter No. :  ");
+                int no = Convert.ToInt32(Console.ReadLine());
+                int count = 0;
+                if (no <= 0)
+                    throw new Exception("No. should be greater than 0");
+                while (no > 0)
+                {
+                    if (no % 2 != 0)
+                        count++;
+                    no /= 2;
+                }
+                Console.WriteLine("No. of 1's bit is :" + count);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
