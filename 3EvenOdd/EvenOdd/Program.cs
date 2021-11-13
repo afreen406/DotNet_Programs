49389bb7285f0671b2ec1486c1425113c60019f1
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 18:59:34 +0530

    Add files via upload

---
+++ 3EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of n: ");
+            int n;
+            n= Convert.ToInt32(Console.ReadLine() + "\n");
+
+            Console.WriteLine("Even Numbers :");
+            for (int i = 1; i <=n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine("\nOdd Numbers :");
+            for (int i = 1; i <n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
