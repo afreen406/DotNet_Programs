5bd8e7f9e5bc84e0deda5aef571571df01537d98
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:02:27 +0530

    Add files via upload

---
+++ 9NPrimeNum/NPrimeNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FirstNPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of n \n");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int noOfPrimeCount = 0, count, num = 2;
+            while (noOfPrimeCount < n)
+            {
+                count = 0;
+                for (int i = 2; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                    }
+                    if (count >= 2)
+                    {
+                        break;
+                    }
+                }
+                if (count < 2)
+                {
+                    noOfPrimeCount++;
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+        }
+    }
+}
+
