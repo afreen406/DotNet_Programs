608f25990365d1f5ec7afe5a0afc032722c8530b
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:08:27 +0530

    Add files via upload

---
+++ 22Lcm_Gcd/22Lcm_Gcd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _22Lcm_Gcd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, hcf, st;
+            Console.Write("Enter Num 1 \n");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Num 2 \n");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Num 3 \n");
+            c = Convert.ToInt32(Console.ReadLine());
+           
+
+            st = a < b ? (a < c ? a : c) : (b < c ? b : c);
+
+            for (hcf = st; hcf >= 1; hcf--)
+            {
+                if (a % hcf == 0 && b % hcf == 0 && c % hcf == 0)
+                    break;
+            }
+
+            Console.Write("The Hcf of three Number is {0}", hcf); 
+ 
+        }
+    }
+}
