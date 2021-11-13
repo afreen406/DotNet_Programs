689315de7a65615f943f198479664197922e3312
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:05:38 +0530

    Add files via upload

---
+++ 16ArmstrongNumber/16ArmstrongNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _16ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, n, qsum, r;
+            Console.WriteLine("Armstrong Number from 1 to 500");
+            for (num = 1; num <= 500; num++)
+            {
+                qsum = 0;
+                n = num;
+                while (n != 0)
+                {
+                    r = n % 10;
+                    qsum = qsum + (r * r * r);
+                    n = n / 10;
+                }
+                if (num == qsum)
+                    Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
