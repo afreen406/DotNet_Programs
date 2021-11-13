d16bef52855de81c61b6ae9c012310c40fa7c1f0
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 19:04:28 +0530

    Add files via upload

---
+++ 13AddingDigits/13AddingDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0, reminder;
+           
+            while (number > 0)
+            {
+                reminder = number % 10;
+                sum = sum + reminder;
+                number = number / 10;
+            }
+            
+            Console.WriteLine("The Sum of Digits is : {0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
