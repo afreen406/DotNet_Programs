e16a7ca9dd1fd0eb8c723415d34a389e31e28228
afreen406 <66351684+afreen406@users.noreply.github.com>
2021-11-13 18:57:50 +0530

    Add files via upload

---
+++ 2Sum/Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class GFG
+{
+    static int findSum(int n)
+    {
+        int sum = 0;
+        for (int x = 1; x <= n; x++)
+            sum = sum + x;
+        return sum;
+    }
+    public static void Main()
+    {
+        int n;
+        Console.Write("enter the number \n");
+        n = Convert.ToInt32(Console.ReadLine()+"\n");
+        Console.Write("\n"+findSum(n)+"\n");
+    }
+}
